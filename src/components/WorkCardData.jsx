acd8549f1689b3369f97b271cca80a11607bd39a
Ganiyu Bashir <bashirganiyu@gmail.com>
2023-10-01 19:53:04 +0100

    Added linkedin clone

--- src/components/WorkCardData.jsx
+++ src/components/WorkCardData.jsx
@@ -1,6 +1,8 @@
 import Project1 from "../assets/E-commerce.png"
-import Project2 from "../assets/WhatsApp.png"
-import Project3 from "../assets/Education.png"
+import Project2 from "../assets/LinkedIn.png"
+import Project3 from "../assets/WhatsApp.png"
+import Project4 from "../assets/Education.png"
+
 
 const ProjectCardData = [
     
@@ -12,16 +14,22 @@ const ProjectCardData = [
     },
     {
         imgsrc: Project2,
+        title: "An Clone of LinkedIn",
+        text: "In this project I was able to build a Full stack clone of LinkedIn using ReactJS. It allows for User Authentication and real-time messaging using firebase. I learnt a lot making this build.",
+        view: "https://linkedin-clone-60d10.web.app/",
+    },
+    {
+        imgsrc: Project3,
         title: "A clone of WhatsApp",
         text: "This project only shows the frontend of WhatsApp clone. I built a full-stack of this project using MERN.",
         view: "https://bigmanbash.github.io/WhatsApp-Replica/",
     },  
     {
-        imgsrc: Project3,
+        imgsrc: Project4,
         title: "An Educational Website",
         text: "This is one of the first projects I ever did in web development. The only one in my portfolio website built with only HTML and CSS. The design is still sleek.",
         view: "https://bigmanbash.github.io/clone-of-unilorin-site/",
-    }
+    }   
 ]
 
 export default ProjectCardData;
